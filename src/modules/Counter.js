6b7ab5b812c24636e640ee52981aa10cbd5fa971
jjs951213 <jjs951213@naver.com>
2020-09-13 02:18:49 +0900

    17.6 immer

--- src/modules/Counter.js
+++ src/modules/Counter.js
@@ -1,14 +1,11 @@
-import { createAction ,  handleActions } from 'redux-actions';
+import { createAction, handleActions } from 'redux-actions';
 
+//액션 타입 정의하기
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
 
 export const increase = createAction(INCREASE);
 export const decrease = createAction(DECREASE);
-
-const initialState = {
-  number: 0,
-};
 
 const counter = handleActions(
   {
