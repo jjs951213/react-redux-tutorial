d847f7313840e43b1484818091ddde17d2d6fbb2
jjs951213 <jjs951213@naver.com>
2020-09-13 01:52:11 +0900

    17.5.1-2 CounterContainer 만들기 마무리

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -4,27 +4,17 @@ import Counter from '../components/Counter';
 import { increase, decrease } from '../modules/counter';
 
 const CounterContainer = ({ number, increase, decrease }) => {
-    return (
-      <Counter number={number} onIncrease={increase} onDecrease={decrease} />
-    );
-  };
-  
-  
-  
-  const mapStateToProps = state => ({
+  return (
+    <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+  );
+};
+
+export default connect(
+  state => ({
     number: state.counter.number,
-  });
-  const mapDispatchToProps = dispatch => ({
-    increase: () => {
-      dispatch(increase());
-    },
-    decrease: () => {
-      dispatch(decrease());
-    },
-  });
-  export default connect(
-    mapStateToProps,
-    mapDispatchToProps,
-  )(CounterContainer);
-  
-  
+  }),
+  {
+    increase,
+    decrease,
+  },
+)(CounterContainer);
